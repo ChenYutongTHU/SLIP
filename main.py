6c33978b80135fb473e4853226f1266d619c70a7
chenyutong <chenytjudy@gmail.com>
2022-10-18 03:20:02 +0000

    bugfix

--- main.py
+++ main.py
@@ -592,6 +592,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     os.makedirs(args.output_dir, exist_ok=True)
     if not os.path.isfile(args.root) and '/data11/private/chenyutong/data' in args.root:
-        args.root = args.root.replace('/data11/private/chenyutong/data','../../../data/cc3m/')
+        args.root = args.root.replace('/data11/private/chenyutong/data','../../../data')
         print(f'training on itp instead of on thunlp, args.root={args.root}')
     main(args)
